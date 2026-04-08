8f919c6b38d1e699a142e706dfa16b72fac8e1ee
SkyLockatf1 <g2487701324@gmail.com>
2026-04-08 15:18:39 +0800

    調整隨機字母生成的長度，增加生成字母的範圍

--- ex6/script.js
+++ ex6/script.js
@@ -2,7 +2,7 @@ var container = document.getElementById("container");
 window.onload = (e)=>{
     let result = '';
     const characters = 'abcdefghijklmnopqrstuvwxyz';
-    const length = Math.floor(Math.random()*2);
+    const length = Math.floor(Math.random()*3);
     // Loop to generate characters for the specified length
     for (let i = 0; i < length; i++) {
         const randomInd = Math.floor(Math.random() * characters.length);
@@ -21,7 +21,7 @@ window.addEventListener("keyup",(e)=>{
 function add_new_char(){
     let result = '';
     const characters = 'abcdefghijklmnopqrstuvwxyz';
-    const length = Math.floor(Math.random()*(3-1)+1);
+    const length = Math.floor(Math.random()*(4-1)+1);
     // Loop to generate characters for the specified length
     for (let i = 0; i < length; i++) {
         const randomInd = Math.floor(Math.random() * characters.length);
