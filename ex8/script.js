dce70027e6f5557d961ae8d84f48e71410951e99
SkyLockatf1 <g2487701324@gmail.com>
2026-04-15 16:25:53 +0800

    新增 ex8 資料夾，包含 index.html 和 script.js 檔案，實現景點觀光展覽資訊的動態顯示功能

---
+++ ex8/script.js
@@ -0,0 +1,26 @@
+var openUrl ="https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=6"
+var xhr=new XMLHttpRequest();
+xhr.open('GET',openUrl,true);
+xhr.send();
+xhr.onreadystatechange = function (){
+    if(this.readyState ==4 && this.status==200){
+        dataset = JSON.parse(this.responseText);
+        addNewData(dataset);
+    }
+};
+function addNewData(dataset){
+    let myTable = document.getElementById("csie");
+    dataset.forEach(function(data,index){
+        let row = myTable.insertRow(-1);
+        row.insertCell(0).innerHTML = data['title'];
+        row.insertCell(1).innerHTML = data['showInfo'][0]['location'];
+        row.insertCell(2).innerHTML = data['showInfo'][0]['price'];
+    });
+}
+function delOldData(){
+    let myTable = document.getElementById("csie");
+    let rowCount = myTable.rows.length;
+    if(rowCount>1){
+        let row = myTable.deleteRow(rowCount-1);
+    }
+}
